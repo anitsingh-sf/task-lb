a6e718d5c9e1f54e2eedf1d68cd5bf112c4a434e
Anit Singh <anit.singh@sourcefuse.com>
2020-05-01 15:15:23 +0530

    Previous assignment to loopback app.

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,82 @@
+import {Filter, repository} from '@loopback/repository';
+import {del, get, getModelSchemaRef, param, post, put, requestBody} from '@loopback/rest';
+import {Users} from '../models';
+import {UsersRepository} from '../repositories';
+
+export class UsersController {
+  constructor(
+    @repository(UsersRepository)
+    public usersRepository: UsersRepository,
+  ) {}
+
+  @post('/users', {
+    responses: {
+      '200': {
+        description: 'Users model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Users)}},
+      },
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Users, {
+            title: 'NewUsers',
+            exclude: ['index'],
+          }),
+        },
+      },
+    })
+    users: Omit<Users, 'index'>,
+  ): Promise<Users> {
+    return this.usersRepository.create(users);
+  }
+
+  @get('/users', {
+    responses: {
+      '200': {
+        description: 'Array of Users model instances',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: getModelSchemaRef(Users, {includeRelations: true}),
+            },
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Users) filter?: Filter<Users>,
+  ): Promise<Users[]> {
+    return this.usersRepository.find(filter);
+  }
+
+  @put('/users/{id}', {
+    responses: {
+      '204': {
+        description: 'Users PUT success',
+      },
+    },
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() users: Users,
+  ): Promise<void> {
+    users.updatedon = new Date();
+    await this.usersRepository.replaceById(id, users);
+  }
+
+  @del('/users/{id}', {
+    responses: {
+      '204': {
+        description: 'Users DELETE success',
+      },
+    },
+  })
+  async deleteById(@param.path.number('id') id: number): Promise<void> {
+    await this.usersRepository.deleteById(id);
+  }
+}
---
+++ src/models/users.model.ts
@@ -0,0 +1,85 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Users extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  index?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstname: string;
+
+  @property({
+    type: 'string',
+    allowNull: true
+  })
+  middlename?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastname: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phone: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  role: number;
+
+  @property({
+    type: 'string',
+    allowNull: true
+  })
+  address?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  customer: number;
+
+  @property({
+    type: 'date',
+  })
+  createdon: Date;
+
+  @property({
+    type: 'date',
+  })
+  updatedon: Date;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Users>) {
+    super(data);
+  }
+}
+
+export interface UsersRelations {
+  // describe navigational properties here
+}
+
+export type UsersWithRelations = Users & UsersRelations;
